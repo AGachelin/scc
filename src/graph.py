57efdece4c7093268a98ddf090f6a5930546b577
mana-byte <manaikilaut@gmail.com>
2026-02-20 16:24:03 +0100

    aled

--- src/graph.py
+++ src/graph.py
@@ -4,6 +4,10 @@ import openai
 from yaml import load, FullLoader
 from bertopic import BERTopic
 from bertopic.representation import OpenAI
+import matplotlib.pyplot as plt
+import os
+
+os.makedirs("./plots", exist_ok=True)
 
 URL_API = "https://openrouter.ai/api/v1"
 creds = load(open("./env.yaml"), Loader=FullLoader)
@@ -18,5 +22,9 @@ representation_model = OpenAI(openai_client, model="gpt-4o-mini", chat=True)
 topic_model = BERTopic(representation_model=representation_model)
 topics, probs = topic_model.fit_transform(documents=docs)
 
+# Results
 info = topic_model.get_topic_info()
 print(info)
+
+plt_fig = plt.figure()
+fig = topic_model.visualize_hierarchy()
