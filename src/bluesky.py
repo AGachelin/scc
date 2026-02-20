c2f2293a5aa53887ac8f7ac59592d66c27508a3c
AGachelin <159674898+AGachelin@users.noreply.github.com>
2026-02-20 14:36:22 +0100

    cleaned code

--- src/bluesky.py
+++ src/bluesky.py
@@ -4,77 +4,9 @@ from atproto import Client
 from yaml import load, FullLoader
 import pandas as pd
 
-creds = load(open("env.yaml"), Loader=FullLoader)
+creds = load(open("../env.yaml"), Loader=FullLoader)
 client = Client()
 client.login(creds["BLUESKY_EMAIL"], creds["BLUESKY_PASSWORD"])
-
-url = "https://bsky.app/profile/bdecourson.bsky.social/post/3meiwcx6jts26"
-handle = "bdecourson.bsky.social"
-post_id = "3meiwcx6jts26"
-
-
-
-user_info = client.resolve_handle(handle)
-did = user_info.did
-
-post_uri = f"at://{did}/app.bsky.feed.post/{post_id}"
-post = client.app.bsky.feed.get_post_thread({"uri":post_uri})
-json_post = post.model_dump()
-
-with open("data/post.json", "w") as f:
-    f.write(json.dumps(json_post, indent=4))
-
-
-
-reposted = client.app.bsky.feed.get_reposted_by({"uri":post_uri})
-len(reposted.model_dump()["reposted_by"])
-
-
-all_posts = []
-cursor = None
-
-while True:
-    response = client.app.bsky.feed.get_author_feed({"actor":did, "limit":100, "cursor":cursor})
-    all_posts.extend(response["feed"])
-    cursor = response.cursor
-    if not cursor:
-        break  # Fin de la pagination
-
-print(f"Nombre total de posts récupérés : {len(all_posts)}")
-
-# Optionnel : Afficher les 5 premiers posts
-for post in all_posts[:5]:
-    print(post["post"]["record"]["text"])
-
-user_info = client.resolve_handle("clairem.secondlife.bio")
-did = user_info.did
-feed_id = "aaajf4zkrdcte"
-feed_uri = f"at://{did}/app.bsky.feed.generator/{feed_id}"
-print(feed_uri)
-
-# Retrieve all posts from the feed
-all_posts = []
-cursor = None
-
-while True:
-    # Request feed posts with pagination
-    response = client.app.bsky.feed.get_feed({"feed":feed_uri, "limit":100, "cursor":cursor})
-
-    # Add posts to the list
-    all_posts.extend(response["feed"])
-
-    # Handle pagination
-    cursor = response.cursor
-    if not cursor:
-        break  # No more posts to fetch
-
-# Print total posts retrieved
-print(f"Total posts retrieved from feed '{feed_uri}': {len(all_posts)}")
-
-print(all_posts[10].dict()["post"]["record"]["text"])
-
-
-
 
 all_posts = []
 cursor = None
@@ -88,12 +20,6 @@ while True:
     if len(all_posts) >= n_elements:
         break
     print(len(all_posts))
-
-
-post = all_posts[1100]
-
-dump = post.model_dump()
-
 
 df = pd.DataFrame([[post.record.created_at, post.record.text, post.author.did, 
                     post.author.handle, post.author.display_name, post.author.created_at, 
