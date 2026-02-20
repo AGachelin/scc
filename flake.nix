9b62d291cd894779ebaca6a26e4b56f72a5ab622
mana-byte <manaikilaut@gmail.com>
2026-02-20 14:23:48 +0100

    flake: add

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Develop Python on Nix with uv";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    inherit (nixpkgs) lib;
+    forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.stdenv.cc.cc pkgs.libz];
+        };
+      }
+    );
+  };
+}
